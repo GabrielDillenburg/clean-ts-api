83e5be875db83c6854b4f8b46cea3888c31bfd76
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2021-12-05 21:40:25 -0300

    fix: change extension from jest config files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
 
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
