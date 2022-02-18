fbfde7de51759515fbc7893aa9c3ce03b5197805
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-18 12:08:43 -0300

    feat: ensure mondoDb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -10,20 +10,22 @@ export const MongoHelper = {
   },
 
   async disconnect (): Promise<void> {
-    await this.client.close()
-    this.client = null
+    await this.client?.close()
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.client.connect(this.uri)
+    }
     return this.client.db().collection(name)
-  },
-
-  map: (data: any): any => {
-    const { _id, ...rest } = data
-    return { ...rest, id: _id.toHexString() }
-  },
-
-  mapCollection: (collection: any[]): any[] => {
-    return collection.map(c => MongoHelper.map(c))
   }
+
+  // map: (data: any): any => {
+  //   const { _id, ...rest } = data
+  //   return { ...rest, id: _id.toHexString() }
+  // },
+
+  // mapCollection: (collection: any[]): any[] => {
+  //   return collection.map(c => MongoHelper.map(c))
+  // }
 }
