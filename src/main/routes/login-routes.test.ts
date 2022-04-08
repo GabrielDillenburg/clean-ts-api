2ac067f52b7d458fa2d967d2222b652bf56c132d
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-04-08 19:53:51 -0300

    test: login route returns 401 with invalid credentials=

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
